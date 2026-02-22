b7bb58b2c5fe4dbe8644e4e00eb97b3be3bbf09d
bkovalick <35934316+bkovalick@users.noreply.github.com>
2026-02-21 21:10:02 -0500

    Update signals_config.py

--- src/models/signals_config.py
+++ src/models/signals_config.py
@@ -5,3 +5,13 @@ from typing import Dict
 class SignalsConfig:
     apply_winsorizing: bool
     windsor_percentiles: Dict
+    
+    def from_dict(cls, d: dict):
+        return cls(
+            apply_winsorizing = d.get("apply_windsorizing", True),
+            windsor_percentiles = d.get(
+                "windsor_percentiles",
+                {"lower": 0.05, "upper": 0.95}
+            )
+        )
+        
